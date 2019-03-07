e7845794f5d729138dd6bea8ff9a2aa33963933b
Chase Paules <cpaules@gmail.com>
2019-03-06 22:49:52 -0500

    fixed edge case for missing authors array

--- src/BookCard.js
+++ src/BookCard.js
@@ -3,11 +3,11 @@ import React from 'react';
 
 const BookCard = ({ book }) => (
     <div key={book.id} className="BookCard">
-      <h3>{book.volumeInfo.title}</h3>
-      <h5>{book.volumeInfo.authors[0]}</h5>
+      {console.log(book)}
+      <h3>{book.volumeInfo.title} </h3>
+      <h5>{book.volumeInfo.authors ? book.volumeInfo.authors[0] : ""}</h5>
       <img className="BookImg" src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title}/>
-      <p className="BookTitle">{book.searchInfo ? book.searchInfo.textSnippet : ""}</p>
-      {console.log(book.searchInfo ? book.searchInfo.textSnippet : "")}
+      <p className="BookTitle">{book.searchInfo ? book.searchInfo.textSnippet : ""} </p>   
     </div>
   )
   
